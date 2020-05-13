1fb42b63c6b57755490a5456ccb35149c6ed6caa
BackYard911 <yousseftarek9801@gmail.com>
2020-05-13 04:15:25 +0200

    added MSAGL reqs

---
+++ Compiler/MSGAL_Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Compiler
+{
+    public partial class MSGAL_Test : Form
+    {
+        
+        public MSGAL_Test()
+        {
+            InitializeComponent();
+        }
+    }
+}
